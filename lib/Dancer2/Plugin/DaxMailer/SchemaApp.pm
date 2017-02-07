fffbba3b99fdcce322dc00f84c7adbaffd4d6c87
John Barrett <john@jbrt.org>
2017-02-07 13:14:30 +0000

    Fixing SchemaApp - plugin keywords are no longer exported in DSL

--- lib/Dancer2/Plugin/DaxMailer/SchemaApp.pm
+++ lib/Dancer2/Plugin/DaxMailer/SchemaApp.pm
@@ -7,8 +7,6 @@ use Dancer2::Plugin;
 on_plugin_import {
     my ( $dsl ) = @_;
     my $plugin = $dsl->app->with_plugin('DBIC');
-    $plugin->schema;
-    #die "No schema method in app. Did you load DBIC::Plugin::DBIC before DaxMailer::Web::Plugin::SchemaApp?" if !( );
     $plugin->schema->can('app') && $plugin->schema->app($dsl);
 };
 
