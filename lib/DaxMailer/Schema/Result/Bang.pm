bc830bd7461935a23b147572a577c8bbcf56d8e0
John Barrett <john@jbrt.org>
2017-02-16 15:39:03 +0000

    Comment, site name and category modelling

--- lib/DaxMailer/Schema/Result/Bang.pm
+++ lib/DaxMailer/Schema/Result/Bang.pm
@@ -11,7 +11,9 @@ primary_column command => { data_type => 'text' };
 primary_column url     => { data_type => 'text' };
 
 column email_address   => { data_type => 'text', is_nullable => 1 };
-
+column comments        => { data_type => 'text', is_nullable => 1 };
+column site_name       => { data_type => 'text' };
+column category_id     => { data_type => 'integer' };
 
 might_have subscriber => 'DaxMailer::Schema::Result::Subscriber' => sub {
     my ( $self, $foreign ) = @{ $_[0] }{qw/ self_alias foreign_alias /};
---
+++ lib/DaxMailer/Schema/Result/Bang/Category.pm
@@ -0,0 +1,20 @@
+package DaxMailer::Schema::Result::Bang::Category;
+
+use Moo;
+extends 'DaxMailer::Schema::Result';
+use DBIx::Class::Candy;
+use Email::Valid;
+
+table 'bang_category';
+
+primary_column id => { data_type => 'int', is_auto_increment => 1 };
+
+column live       => { data_type => 'int', default_value => 1 };
+column name       => { data_type => 'text' };
+column parent     => { data_type => 'int' };
+
+might_have parent_category => 'DaxMailer::Schema::Result::Bang::Category' => {
+    'foreign.id' => 'self.id'
+};
+
+1;
