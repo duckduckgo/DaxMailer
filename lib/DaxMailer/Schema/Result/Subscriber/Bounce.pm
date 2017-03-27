64bf3d35bf607966680a445ab48a164bc241aeb8
John Barrett <john@jbrt.org>
2017-03-27 19:02:51 +0100

    Remove subscriber <-> bounces rel

--- lib/DaxMailer/Schema/Result/Subscriber/Bounce.pm
+++ lib/DaxMailer/Schema/Result/Subscriber/Bounce.pm
@@ -11,8 +11,4 @@ primary_column email_address => { data_type => 'text' };
 column bounced      => { data_type => 'int', default_value => 0 };
 column complaint    => { data_type => 'int', default_value => 0 };
 
-has_many subscribers => 'DaxMailer::Schema::Result::Subscriber' => {
-    'foreign.email_address' => 'self.email_address',
-};
-
 1;
