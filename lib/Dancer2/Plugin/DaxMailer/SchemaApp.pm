f8d3ebb18c2d453be4442406e7b03679398083a3
John Barrett <john@jbrt.org>
2017-03-28 11:00:03 -0400

    We need the dancer core app instance here, not the plugin DSL

--- lib/Dancer2/Plugin/DaxMailer/SchemaApp.pm
+++ lib/Dancer2/Plugin/DaxMailer/SchemaApp.pm
@@ -7,7 +7,7 @@ use Dancer2::Plugin;
 on_plugin_import {
     my ( $dsl ) = @_;
     my $plugin = $dsl->app->with_plugin('DBIC');
-    $plugin->schema->can('app') && $plugin->schema->app($dsl);
+    $plugin->schema->can('app') && $plugin->schema->app($dsl->app);
 };
 
 register_plugin;
