376bc6a7251e7cbe0f1e52b78cab06922f969cb2
John Barrett <john@jbrt.org>
2017-04-19 12:48:15 -0400

    Script to migrate ddgc subscribers to daxmailer

---
+++ lib/DaxMailer/Script/MigrateDDGC.pm
@@ -0,0 +1,50 @@
+use strict;
+use warnings;
+package DaxMailer::Script::MigrateDDGC;
+
+use Moo;
+use DBI;
+use Carp;
+
+with 'DaxMailer::Base::Script::Service';
+
+has ddgc_dbh => ( is => 'lazy' );
+sub _build_ddgc_dbh {
+    my ( $self ) = @_;
+    my $dbh = DBI->connect( @{ config() }{qw/
+        legacy_db_dsn
+        legacy_db_user
+        legacy_db_password
+    /} );
+    croak "Unable to open legacy db" unless $dbh;
+    return $dbh;
+}
+
+sub go {
+    my ( $self ) = @_;
+    my $sth = $self->ddgc_dbh->prepare('SELECT * FROM subscriber');
+    $sth->execute;
+    while ( my $row = $sth->fetchrow_hashref ) {
+        my $email = Email::Valid->address($row->{email_address});
+        next unless $email;
+        rset('Subscriber')->update_or_create({
+            email_address => $email,
+            unsubscribed  => $row->{unsubscribed},
+            verified      => $row->{verified},
+            campaign      => $row->{campaign},
+            v_key         => $row->{v_key},
+            u_key         => $row->{u_key},
+            flow          => $row->{flow},
+            extra         => {},
+        });
+        if ( $row->{bounced} || $row->{complaint} ) {
+            rset('Subscriber::Bounce')->update_or_create({
+                email_address => $email,
+                bounced       => $row->{bounced},
+                complaint     => $row->{complaint},
+            });
+        }
+    }
+}
+
+1;
