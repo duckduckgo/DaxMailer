9c85db3147018c28d95080185aabbd7ba7846578
John Barrett <john@jbrt.org>
2017-04-21 07:22:21 -0400

    Check a verify mailrun happened in the last N hours
    Default 2

---
+++ lib/DaxMailer/Script/VerifyLastSend.pm
@@ -0,0 +1,28 @@
+use strict;
+use warnings;
+package DaxMailer::Script::VerifyLastSend;
+
+use Moo;
+use MooX::Options;
+
+use DateTime;
+use DateTime::Duration;
+
+with 'DaxMailer::Base::Script::Service';
+
+has hours => (
+    is => 'ro',
+    default => sub { 2 },
+);
+
+sub go {
+    my $x_hours_ago = schema('default')->storage->datetime_parser->format_datetime(
+        DateTime->now->subtract( hours => $_[0]->hours )
+    );
+    my $sent = rset('Subscriber::MailLog')->search({
+        sent => { '>=' => $x_hours_ago }
+    })->one_row;
+    exit ( $sent ? 0 : 2 );
+}
+
+1;
