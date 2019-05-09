ce71b76e32fb730181bcc27ac56226d8f93ea9ab
John Barrett <john@jbrt.org>
2019-05-09 09:33:53 -0400

    Set updated timestamp on create

--- lib/DaxMailer/Schema/Result/Subscriber/Mailtrain.pm
+++ lib/DaxMailer/Schema/Result/Subscriber/Mailtrain.pm
@@ -9,7 +9,7 @@ table 'subscriber_mailtrain';
 primary_column email_address => { data_type => 'text' };
 primary_column operation     => { data_type => 'text' };
 
-column updated => { data_type => 'timestamptz', set_on_update => 1 };
+column updated => { data_type => 'timestamptz', set_on_create => 1, set_on_update => 1 };
 column created => { data_type => 'timestamptz', set_on_create => 1 };
 
 column processed => { data_type => 'int', default_value => 0 };
