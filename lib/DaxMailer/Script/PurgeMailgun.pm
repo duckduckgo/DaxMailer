1ce8c215679f84a24af0fe0188689c938fbd3547
Maria Grazia Alastra <maria@duckduckgo.com>
2021-07-06 10:39:41 -0400

    Printing debug info, catching errors, deleting one address at a time (mailgun commented out for now)

--- lib/DaxMailer/Script/PurgeMailgun.pm
+++ lib/DaxMailer/Script/PurgeMailgun.pm
@@ -6,6 +6,7 @@ use Moo;
 use HTTP::Request;
 use LWP::UserAgent;
 use JSON;
+use Try::Tiny;
 
 with 'DaxMailer::Base::Script::Service';
 
@@ -15,14 +16,15 @@ sub go {
     my $api_domain = $ENV{DAXMAILER_MAILGUN_API_DOMAIN};
     my $api_key = $ENV{DAXMAILER_MAILGUN_API_KEY};
     my $base_uri = "https://api.mailgun.net/v3/$api_domain";
-    my $api_uris = [
-        "$base_uri/bounces";
-        "$base_uri/complaints";
-        "$base_uri/unsubscribes";
-    ]
+    my @api_uris = (
+        "$base_uri/bounces",
+        "$base_uri/complaints",
+        "$base_uri/unsubscribes"
+    );
 
-    for my $uri in ( @api_uris ) {
-        $self->_purge_mailgun(uri, $api_key);
+    for my $uri ( @api_uris ) {
+        print "Calling _purge_mailgun() with uri $uri";
+        $self->_purge_mailgun($uri, $api_key);
     }
 }
 
@@ -36,12 +38,18 @@ sub _purge_mailgun {
               'WWW-Authenticate' => "api:$api_key"
               ] );
     my $ua = LWP::UserAgent->new();
-    my $res_bounces = $ua->request($req_bounces);
-
-    push(@bounces, from_json($res_bounces)->items);
+    my $res_bounces;
+    
+    try {
+        $res_bounces = $ua->request($req_bounces);
+        push(@bounces, from_json($res_bounces)->items);
+    } catch {
+        warn "Error trying to fetch data for $api_uri: $_";
+    };
 
     for my $bounced ( @bounces ) {
         my $address = $bounced->address;
+        print "Purging email address $address from local DB";
 
         # Delete from local and Mailtrain DBs
         rset('Subscriber::Mailtrain')->unsubscribe( $address );
@@ -49,14 +57,22 @@ sub _purge_mailgun {
             email_address => $address,
             campaign => 'b' 
         } )->delete();
+
+        # Delete bounced address from Mailgun
+        #print "Delete $address from Mailgun";
+        #my $req_delete = HTTP::Request->new( 'DELETE', "$api_uri/$address", [
+        #          'WWW-Authenticate' => "api:$api_key"
+        #          ] );
+        #my $ua = LWP::UserAgent->new();
+        #my $res_delete;
+        #
+        #try {
+        #    $res_delete = $ua->request($req_delete);
+        #} catch {
+        #    warn "Error trying to delete $address: $_";
+        #};
     }
 
-    # Delete bounce list from Mailgun
-    my $req_delete = HTTP::Request->new( 'DELETE', $api_uri, [
-              'WWW-Authenticate' => "api:$api_key"
-              ] );
-    my $ua = LWP::UserAgent->new();
-    my $res_delete = $ua->request($req_delete);
 }
 
 1;
